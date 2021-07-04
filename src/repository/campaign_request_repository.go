6f708eaf43d1eb2c212c7419c9b23b334d221ee5
st3fan7 <stefan.aradjanin@gmail.com>
2021-07-04 04:44:42 +0200

    :bug: Fixed requests ids

--- src/repository/campaign_request_repository.go
+++ src/repository/campaign_request_repository.go
@@ -7,14 +7,14 @@ import (
 	"github.com/google/uuid"
 )
 const (
-	CreateDisposableRequestTable = "CREATE TABLE IF NOT EXISTS adpost_keyspace.DisposableCampaignRequest (id text, influencer_id text, campaign_id text, status int, PRIMARY KEY(influencer_id, id));"
-	CreateMultipleRequestTable = "CREATE TABLE IF NOT EXISTS adpost_keyspace.MultipleCampaignRequest (id text, influencer_id text, campaign_id text, status int, PRIMARY KEY(influencer_id, id));"
-	InsertIntoDisposableRequestTable = "INSERT INTO adpost_keyspace.DisposableCampaignRequest (id, influencer_id, campaign_id, status) VALUES (?, ?, ?, ?) IF NOT EXISTS;"
-	InsertIntoMultipleRequestTable = "INSERT INTO adpost_keyspace.MultipleCampaignRequest (id, influencer_id, campaign_id, status) VALUES (?, ?, ?, ?) IF NOT EXISTS;"
+	CreateDisposableRequestTable = "CREATE TABLE IF NOT EXISTS adpost_keyspace.DisposableCampaignRequest (id text, influencer_id text, agent_id text, campaign_id text, status int, PRIMARY KEY(influencer_id, id));"
+	CreateMultipleRequestTable = "CREATE TABLE IF NOT EXISTS adpost_keyspace.MultipleCampaignRequest (id text, influencer_id text, agent_id text, campaign_id text, status int, PRIMARY KEY(influencer_id, id));"
+	InsertIntoDisposableRequestTable = "INSERT INTO adpost_keyspace.DisposableCampaignRequest (id, influencer_id, agent_id, campaign_id, status) VALUES (?, ?, ?, ?, ?) IF NOT EXISTS;"
+	InsertIntoMultipleRequestTable = "INSERT INTO adpost_keyspace.MultipleCampaignRequest (id, influencer_id, agent_id, campaign_id, status) VALUES (?, ?, ?, ?, ?) IF NOT EXISTS;"
 	UpdateDisposableRequestStatus = "UPDATE adpost_keyspace.DisposableCampaignRequest SET status = ? WHERE id = ? AND influencer_id = ?;"
 	UpdateMultipleRequestStatus = "UPDATE adpost_keyspace.MultipleCampaignRequest SET status = ? WHERE id = ? AND influencer_id = ?;"
-	GetAllDisposableCampaignRequestsByInfluencer = "SELECT id, campaign_id, status FROM adpost_keyspace.DisposableCampaignRequest WHERE influencer_id = ?;"
-	GetAllMultipleCampaignRequestsByInfluencer = "SELECT id, campaign_id, status FROM adpost_keyspace.MultipleCampaignRequest WHERE influencer_id = ?;"
+	GetAllDisposableCampaignRequestsByInfluencer = "SELECT id, campaign_id, status, agent_id FROM adpost_keyspace.DisposableCampaignRequest WHERE influencer_id = ?;"
+	GetAllMultipleCampaignRequestsByInfluencer = "SELECT id, campaign_id, status, agent_id FROM adpost_keyspace.MultipleCampaignRequest WHERE influencer_id = ?;"
 
 
 )
@@ -36,11 +36,11 @@ type campaignRequestRepository struct {
 }
 
 func (c campaignRequestRepository) CreateDisposableCampaignRequest(ctx context.Context, request domain.DisposableCampaignRequest) error {
-	return c.cassandraClient.Query(InsertIntoDisposableRequestTable, uuid.NewString(), request.InfluencerId, request.DisposableCampaign.ID, 0).Exec()
+	return c.cassandraClient.Query(InsertIntoDisposableRequestTable, uuid.NewString(), request.InfluencerId, request.AgentId, request.DisposableCampaign.ID, 0).Exec()
 }
 
 func (c campaignRequestRepository) CreateMultipleCampaignRequest(ctx context.Context, request domain.MultipleCampaignRequest) error {
-	return c.cassandraClient.Query(InsertIntoMultipleRequestTable, uuid.NewString(), request.InfluencerId, request.MultipleCampaign.ID, 0).Exec()
+	return c.cassandraClient.Query(InsertIntoMultipleRequestTable, uuid.NewString(), request.InfluencerId, request.AgentId, request.MultipleCampaign.ID, 0).Exec()
 }
 
 func (c campaignRequestRepository) ApproveDisposableCampaignRequest(ctx context.Context, request domain.DisposableCampaignRequest) error {
@@ -60,32 +60,32 @@ func (c campaignRequestRepository) RejectMultipleCampaignRequest(ctx context.Con
 }
 
 func (c campaignRequestRepository) GetAllDisposableCampaignRequests(ctx context.Context, userId string) ([]domain.DisposableCampaignRequest, error) {
-	var id, campaignId string
+	var id, campaignId, agentId string
 	var status int
 	iter := c.cassandraClient.Query(GetAllDisposableCampaignRequestsByInfluencer, userId).Iter().Scanner()
 	var retVal []domain.DisposableCampaignRequest
 	for iter.Next() {
-		err := iter.Scan(&id, &campaignId, &status)
+		err := iter.Scan(&id, &campaignId, &status, &agentId)
 		if err != nil {
 			continue
 		}
-		retVal = append(retVal, domain.DisposableCampaignRequest{Id: id, InfluencerId: userId, DisposableCampaign: domain.DisposableCampaign{ID: campaignId}, RequestStatus: domain.RequestStatus(status)})
+		retVal = append(retVal, domain.DisposableCampaignRequest{Id: id, InfluencerId: userId, AgentId: agentId, DisposableCampaign: domain.DisposableCampaign{ID: campaignId}, RequestStatus: domain.RequestStatus(status)})
 	}
 
 	return retVal, nil
 }
 
 func (c campaignRequestRepository) GetAllMultipleCampaignRequests(ctx context.Context, userId string) ([]domain.MultipleCampaignRequest, error) {
-	var id, campaignId string
+	var id, campaignId, agentId string
 	var status int
 	iter := c.cassandraClient.Query(GetAllMultipleCampaignRequestsByInfluencer, userId).Iter().Scanner()
 	var retVal []domain.MultipleCampaignRequest
 	for iter.Next() {
-		err := iter.Scan(&id, &campaignId, &status)
+		err := iter.Scan(&id, &campaignId, &status, &agentId)
 		if err != nil {
 			continue
 		}
-		retVal = append(retVal, domain.MultipleCampaignRequest{Id: id, InfluencerId: userId, MultipleCampaign: domain.MultipleCampaign{ID: campaignId}, RequestStatus: domain.RequestStatus(status)})
+		retVal = append(retVal, domain.MultipleCampaignRequest{Id: id, InfluencerId: userId, AgentId: agentId, MultipleCampaign: domain.MultipleCampaign{ID: campaignId}, RequestStatus: domain.RequestStatus(status)})
 	}
 
 	return retVal, nil
